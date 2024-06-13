6af1c2aa446e8033ecc369c98257172a61c97ca1
Dhrishita <97584165+Dhrishita@users.noreply.github.com>
2024-06-13 08:14:12 +0530

    Time: 9 ms (42.72%), Space: 5.7 MB (96.85%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.c
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.c
@@ -4,7 +4,7 @@ int compare(const void *a,const void *b)
 }
 int minMovesToSeat(int* seats, int seatsSize, int* students, int studentsSize) {
     qsort(seats,seatsSize,sizeof(int),compare);
-     qsort(students,studentsSize,sizeof(int),compare);
+    qsort(students,studentsSize,sizeof(int),compare);
     int Min_moves =0;
     for(int i=0;i<seatsSize;i++)
     {
