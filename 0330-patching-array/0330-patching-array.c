eecd9d185ff2443a7dc99d85ade68b2f256121d8
Dhrishita <97584165+Dhrishita@users.noreply.github.com>
2024-06-16 09:36:36 +0530

    Time: 3 ms (68.75%), Space: 5.9 MB (93.75%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.c
@@ -0,0 +1,21 @@
+int minPatches(int* nums, int numsSize, int n) {
+    long long maxsum = 0; 
+    int patches = 0; 
+    int i = 0;
+
+    while(maxsum<n) 
+    {
+        if(i<numsSize && nums[i]<=maxsum+1)
+        {
+            maxsum+= nums[i];
+            i++;
+        }
+        else 
+        {
+            maxsum+= maxsum+1; 
+            patches++;
+        }
+    }
+    return patches;
+}
+
