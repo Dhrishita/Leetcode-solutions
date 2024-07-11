c819c75bb1c4cc8b69ab43f5665d7ced49a1f5a0
Dhrishita <97584165+Dhrishita@users.noreply.github.com>
2024-07-11 09:26:11 +0530

    Time: 22 ms (82.46%), Space: 12.1 MB (6.43%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minOperations(self, logs):
+        ans=0
+        for log in logs:
+            if log=='./':
+                continue
+            elif log=='../':
+                ans=max(0, ans - 1)
+            else:
+                ans+=1
+        return ans
