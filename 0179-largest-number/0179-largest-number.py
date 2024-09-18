9d4110697fbd1a1531f6cd1a98f29ca2ea3e502f
DHRISHITA <97584165+Dhrishita@users.noreply.github.com>
2024-09-18 22:25:05 +0530

    Time: 33 ms (13.75%), Space: 11.7 MB (40.70%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,25 +1,14 @@
 from functools import cmp_to_key
-
 class Solution(object):
-    def largestNumber(self, nums):
-        # Convert all integers to strings for comparison
-        num_str = list(map(str, nums))
-        
-        # Define a custom comparator function
-        def compare(x, y):
-            # Compare two numbers based on their concatenated values
-            if x + y > y + x:
-                return -1  # x should come before y
-            elif x + y < y + x:
-                return 1  # y should come before x
+    def largestNumber(self,nums):
+        num_str=list(map(str,nums))
+        def compare(D,M):
+            if D+M>M+D:
+                return -1  
+            elif D+M<M+D:
+                return 1  
             else:
-                return 0  # they are equal
-        
-        # Sort numbers using the custom comparator
+                return 0 
         num_str.sort(key=cmp_to_key(compare))
-        
-        # Join the sorted numbers to form the largest number
-        largest = ''.join(num_str)
-        
-        # Edge case: if the largest number is '0', return '0' instead of '000...'
+        largest=''.join(num_str)
         return '0' if largest[0] == '0' else largest
